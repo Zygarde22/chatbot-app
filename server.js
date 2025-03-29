d1bbe22eae63094b80ddf50ef4ef0299125e2cfb
Khris R <khristonrobinson@gmail.com>
2025-03-28 19:02:30 -0500

    Trying to fix some things for deployment

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-app.use(cors());
+app.use(cors()); // This line is necessary to enable CORS
 app.use(express.json());
 
 app.post("/get-api-key", (req, res) => {
-    res.json({ message: process.env.GEMINI_API_KEY });
+    res.json({ apiKey: process.env.GEMINI_API_KEY }); // Send API key in the "apiKey" field
 });
 
 const PORT = process.env.PORT || 3000;
